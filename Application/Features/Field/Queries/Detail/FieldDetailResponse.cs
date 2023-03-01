4bc48b810d4cbed3a05353437f35c2b455a8031d
Dang Hieu Thang <dhthang1998@gmail.com>
2023-03-01 20:50:40 +0700

    Add Enum Slot

--- Application/Features/Field/Queries/Detail/FieldDetailResponse.cs
+++ Application/Features/Field/Queries/Detail/FieldDetailResponse.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Common;
 
 namespace Application.Features.Field.Queries.Detail
 {
@@ -12,7 +13,7 @@ namespace Application.Features.Field.Queries.Detail
         public string Description { get; set; }
         public string Address { get; set; }
         public FieldOwnerResponse Owner { get; set; }
-        public string CategoryName { get; set; }
+        public FieldCategoryResponse Category { get; set; }
         public List<FieldServiceResponse> Services { get; set; }
     }
 
@@ -24,6 +25,8 @@ namespace Application.Features.Field.Queries.Detail
 
     public class FieldCategoryResponse
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
     }
 
     public class FieldOwnerResponse
--- Domain/Common/Enums.cs
+++ Domain/Common/Enums.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,17 @@ namespace Domain.Common
             Booked,
             InActive,
             Active
+        }
+
+        public enum FieldSlot
+        {
+            [Description("6-8H")] H6_8 = 1,
+            [Description("8-10H")] H8_10 = 2,
+            [Description("10-12H")] H10_12 = 3,
+            [Description("12-14H")] H12_14 = 4,
+            [Description("14-16H")] H14_16 = 5,
+            [Description("16-18H")] H16_18 = 6,
+            [Description("18-20H")] H18_20 = 7,
         }
     }
 }
